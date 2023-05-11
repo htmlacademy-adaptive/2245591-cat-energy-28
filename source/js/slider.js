7a599c7ff5e1835b01cfac53fbe9acccba23840b
Schtock <dmitrsuvor@gmail.com>
2023-05-11 19:29:10 +1000

    Оживил слайдер

---
+++ source/js/slider.js
@@ -0,0 +1,25 @@
+const slider = document.querySelector(".compare-images")
+const curtain = slider.querySelector(".compare-images__slider")
+let sliderStyles = getComputedStyle(slider)
+let curtainPlaceStart
+let clientX
+
+window.addEventListener("pointerup", stopTheCurtainShifting)
+curtain.addEventListener("pointerdown", startTheCurtainShifting)
+
+function startTheCurtainShifting (event) {
+	curtainPlaceStart = +(sliderStyles.getPropertyValue("--width"))
+	clientX = event.clientX
+	window.addEventListener("pointermove", shiftТheСurtain)
+}
+
+function shiftТheСurtain (event) {
+	let deltaX = event.clientX - clientX
+	let cursorPlace = curtainPlaceStart + deltaX / slider.clientWidth
+	let curtainPlace = Math.min(Math.max(cursorPlace, 0), 1)
+	slider.style.setProperty("--width", `${curtainPlace}`)
+}
+
+function stopTheCurtainShifting (event) {
+	window.removeEventListener("pointermove", shiftТheСurtain)
+}
